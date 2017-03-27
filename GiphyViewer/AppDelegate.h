ee9e99f61b1ccd3f6b34ba96040e5442034d6e32
Igor Biarozkin <biarozkin@biarozkinsmacbook.local>
2017-03-27 19:39:57 +0300

    Initial Commit

---
+++ GiphyViewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GiphyViewer
+//
+//  Created by Igor Biarozkin on 27.03.17.
+//  Copyright © 2017 Igor Biarozkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GiphyViewer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GiphyViewer
+//
+//  Created by Igor Biarozkin on 27.03.17.
+//  Copyright © 2017 Igor Biarozkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
